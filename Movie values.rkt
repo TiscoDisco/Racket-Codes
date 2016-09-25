fe16e50bc6b88153c2ad4773733609dd218eeecd
Ryan Hamin Kim <ryanwoongae@gmail.com>
2016-09-24 21:11:27 -0400

    Update Movie values.rkt

--- Movie values.rkt
+++ Movie values.rkt
@@ -1,9 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname a02q3) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;Ryan Hamin Kim
-;CS116
-;Assignment 2 Question 3
+
 
 
 (define-struct movie (title year length))
